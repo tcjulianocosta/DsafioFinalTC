024f4c4b538bbdcdc30a10a8f5b6206bfc5cb557
tcjulianocosta <74621280+tcjulianocosta@users.noreply.github.com>
2021-08-21 08:03:27 -0300

    Complemento Desafio TC

--- cypress/support/elements/PreencherOrigemDestinoElements.js
+++ cypress/support/elements/PreencherOrigemDestinoElements.js
@@ -4,11 +4,16 @@ class PreencherOrigemDestinoElements {
     Destino = () => {return '#inputDestination'}
     RioOpcao = () => {return '#ulDestinationAirport > :nth-child(1)'}
     DataIda = () => {return '#_smilesflightsearchportlet_WAR_smilesbookingportlet_departure_date'}
-    DiaIda = () => {return ".ui-datepicker-group-first > .ui-datepicker-calendar > tbody > :nth-child(3) > :nth-child(2) > .ui-state-default"}
+    DiaIda = () => {return '.ui-datepicker-group-first > .ui-datepicker-calendar > tbody > :nth-child(3) > :nth-child(2) > .ui-state-default'}
     DataVolta = () => {return '#_smilesflightsearchportlet_WAR_smilesbookingportlet_return_date'}
     DiaVolta = () => {return ".ui-datepicker-group-last a:contains('29').ui-state-default"}
     BuscarVoo = () => {return '#submitFlightSearch'}
     Milhas = () => {return "cy.get('#firstFlights > .segmentsFlightsTemplate > .smiles__flight-search > #flightsArticle0 > :nth-child(4) > .column-miles > .miles > [style='position:relative'] > .checkbox > .flightlb')"}
-    
+    Checkbox = () => {return ".checkbox > .flightlbest"}
+    ValorMilhas = () => {return ".button > .qtyMiles"}
+    ResumoPedido = () => {return ".underside "}
+    ValorBilhete = () => {return "#smilesSummaryTemplate > div.underside > div > table.table.sub.miles > tbody > tr:nth-child(1) > th:nth-child(2)"}
+    TaxaEmbarque = () => {return ".summary-select-boarding-tax-miles"}
+    TotalPedido = () => {return "#summarytotalmilesId"}
 }
 export default PreencherOrigemDestinoElements;
--- cypress/support/pageobjects/PreencherOrigemDestinoPage.js
+++ cypress/support/pageobjects/PreencherOrigemDestinoPage.js
@@ -7,45 +7,66 @@ import PreencherAddressElements from '../elements/PreencherOrigemDestinoElements
 const preencherAddress = new PreencherAddressElements
 
 //comand+shift+F = Pesquisar em varias abas
-
 class PreencherText {
- 
-//Abrir Browser
 
-    AbrirBrowser (){
+    AbrirBrowser() {
         cy.visit(ambiente)
     }
 
-//Preencher Address
-
-    PrencherOrigemDestino () {
+    PrencherOrigemDestino() {
         cy.get(preencherAddress.Origem()).type('São Paulo (GRU)')
-        cy.get(preencherAddress.Destino()).type('Rio de Janeiro')
+        cy.get(preencherAddress.Destino()).type('Rio de')
         cy.get(preencherAddress.RioOpcao()).should('be.visible').click()
     }
 
-    PrencherData () {
-        
-        cy.get(preencherAddress.DataIda()).click({force:true})
+    PrencherData() {
+        cy.get(preencherAddress.DataIda()).click({ force: true })
         cy.get(preencherAddress.DiaIda()).should('be.visible').click()
 
-        cy.get(preencherAddress.DataVolta()).click({force: true})
+        cy.get(preencherAddress.DataVolta()).click({ force: true })
         cy.get(preencherAddress.DiaVolta()).click()
-
-        cy.get(preencherAddress.BuscarVoo()).click({timout: 200000})
+        cy.get(preencherAddress.BuscarVoo()).click({ timout: 300000 })
     }
 
-    ScrollarTela () {
-
+    ScrollarTela() {
         cy.scrollTo(0, 600)
         cy.get(preencherAddress.Milhas).should('be.visible').click();
-        
     }
 
-    visualizarTest (){
+    SelecionarTarifa() {
+        cy.get('button').should('have.text', 'Selecionar tarifa').click();
+        cy.get(preencherAddress.Checkbox).should('be.visible').click({ timout: 20000 });
+    }
+
+    ReumoPedido() {
+        cy.get(preencherAddress.ResumoPedido).should('be.visible')
+            .and('contain', 'Resumo do pedido')
+
+        cy.get(preencherAddress.ValorMilhas).then(($text) => {
+            const txt = $text.text()
+            cy.log(txt)
+
+            cy.get(preencherAddress.ValorBilhete).then(($text) => {
+                const txt2 = $text.text()
+                cy.log(txt2)
+
+                expect(txt).to.eq(txt2)
+
+                cy.get(preencherAddress.TaxaEmbarque).then(($text) => {
+                    const txt3 = $text.text()
+                    cy.log(txt3)
+
+                    expect(txt3).to.eq(txt + txt2)
+
+                })
+            })
+        })
+    }
+
+    visualizarTest() {
         cy.screenshot
         cy.wait(1000)
     }
-    
+
 }
 export default PreencherText;
